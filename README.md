8ce5a68065200396ffb4217e932609ffb3cb0618
Marie-Christ Sagbo <christsagbo1@gmail.com>
2024-02-14 13:35:04 +0100

    Add

---
+++ README.md
@@ -0,0 +1,1 @@
+# Figma-To-Code-Week4
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors : {
+      'primary-color' : '#000000',
+    },
+    extend: {
+     
+      fontFamily :  {
+        'sans' : ['sans-serif', 'Monstserrat'],
+      }
+     
+    },
+  },
+  plugins: [],
+}
